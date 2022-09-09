1ed1b8fd640ebd9c19f71688ba9425fff3c29ce2
YuliiaRez <qwerty959@gmail.com>
2022-09-09 15:25:36 +0300

    edited

--- index.js
+++ index.js
@@ -11,49 +11,43 @@ function loadGrain(levels) {
   let index2 = levels.length - 1;
 
   if (levels.length < 3) return 0;
-  let i = 0;
-  while (i < levels.length && levels[i] > levels[i + 1]) {
-    index1 = levels.indexOf(levels[i]);
-    i++;
-  }
-  let j = levels.length;
-  while (j > 0 && levels[j] > levels[j - 1]) {
-    index2 = levels.indexOf(levels[j]);
-    i--;
-  }
 
-  if (index1 < index2) {
-    for (let i = index1; i <= index2; i++) {
-      if (max <= levels[i]) {
-        max = levels[i];
-        indexMax = i;
-      }
+  for (let i = index1; i <= index2; i++) {
+    if (max <= levels[i]) {
+      max = levels[i];
+      indexMax = i;
     }
-    side1 = levels[index1];
-    for (let i = index1; i < indexMax; i++) {
-      if (side1 >= levels[i]) {
-        grain = side1 - levels[i];
-      } else {
-        side1 = levels[i];
-        grain = 0;
-      }
-      result += grain;
+  }
+  side1 = levels[index1];
+  for (let i = index1; i < indexMax; i++) {
+    if (side1 >= levels[i]) {
+      grain = side1 - levels[i];
+    } else {
+      side1 = levels[i];
+      grain = 0;
     }
-    side2 = levels[index2];
+    result += grain;
+  }
+  side2 = levels[index2];
 
-    for (let i = index2; i > indexMax; i--) {
-      if (side2 >= levels[i]) {
-        grain = side2 - levels[i];
-      } else {
-        side2 = levels[i];
-        grain = 0;
-      }
-      result += grain;
+  for (let i = index2; i > indexMax; i--) {
+    if (side2 >= levels[i]) {
+      grain = side2 - levels[i];
+    } else {
+      side2 = levels[i];
+      grain = 0;
     }
+    result += grain;
   }
   return result;
 }
+console.log(
+  loadGrain([
+    25, 20, 5, 28, 7,
 
+    42, 47, 9, 41, 37, 41,
+  ])
+); //82
 console.log(loadGrain([4, 1, 3, 1])); // 2;
 console.log(loadGrain([1, 0, 10])); // 1;
 console.log(loadGrain([10, 10])); // 0;
